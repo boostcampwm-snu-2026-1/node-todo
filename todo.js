d164633b7a896574b1a688bfa712e60f03048ab1
장한 <eric2925@snu.ac.kr>
2026-03-08 03:01:33 +0900

    코딩 처음이라 조건문? 이런것도 잘 모르겠음..

--- todo.js
+++ todo.js
@@ -17,3 +17,17 @@ if (command === "add") {
   fs.writeFileSync("todos.json", JSON.stringify(todos));
   console.log("Todo가 추가되었습니다:", content);
 }
+
+if (command === "list") {
+  const data = fs.readFileSync("todos.json", "utf8");
+  const todos = JSON.parse(data);
+
+  if (todos.length === 0) {
+    console.log("Todo가 없습니다.");
+  } else {
+    for (const todo of todos) {
+      const mark = todo.done ? "[x]" : "[ ]";
+      console.log(mark, todo.id + ".", todo.content);
+    }
+  }
+}
