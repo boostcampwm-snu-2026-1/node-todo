6fe8c9a518894ddef191e118145ff722bf64b1cc
장한 <eric2925@snu.ac.kr>
2026-03-08 03:02:57 +0900

    마찬가지로 언어 자체를 잘 모르겠습니다..

--- todo.js
+++ todo.js
@@ -49,3 +49,38 @@ if (command === "done") {
   fs.writeFileSync("todos.json", JSON.stringify(todos));
   console.log(`ID ${id}번 항목이 완료되었습니다.`);
 }
+
+if (command === "delete") {
+  const id = Number(process.argv[3]);
+
+  const data = fs.readFileSync("todos.json", "utf8");
+  const todos = JSON.parse(data);
+
+  const newTodos = todos.filter(todo => todo.id !== id);
+
+  if (todos.length === newTodos.length) {
+    console.log("해당 ID를 찾을 수 없습니다.");
+  } else {
+    fs.writeFileSync("todos.json", JSON.stringify(newTodos));
+    console.log(`ID ${id}번 항목이 삭제되었습니다.`);
+  }
+}
+
+if (command === "update") {
+  const id = Number(process.argv[3]);
+  const newContent = process.argv[4];
+
+  const data = fs.readFileSync("todos.json", "utf8");
+  const todos = JSON.parse(data);
+
+  const todo = todos.find(t => t.id === id);
+
+  if (!todo) {
+    console.log("해당 ID를 찾을 수 없습니다.");
+    return;
+  }
+
+  todo.content = newContent;
+  fs.writeFileSync("todos.json", JSON.stringify(todos));
+  console.log(`ID ${id}번 항목이 수정되었습니다.`);
+}
