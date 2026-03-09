efebd72ff71fa687df13aa6545b953502c3d43b3
dongin <dongin1001@snu.ac.kr>
2026-03-09 11:18:57 +0900

    js에서 객체를 관리하는 방식에 대해 이해하기. 1. {} 중괄호를 사용하여 하나의 오브젝트를 정의할 수 있다. 오브젝트 내에서 key: value 형식으로 각 항목의 default값을 정할 수 있고, 접근 시에는 task.id 등으로 해당 key의 value에 접근하여 수정한다. 2. js에서 변수를 선언할 시에는 let, var등을 사용하여 선언하고 사용해야만 한다. 이 부분이 python과는 다르다. 3. Object배열의 maxid를 확보하는 방법으로 length -1을 사용했다. 이는 python에서 배열의 마지막 element에 접근하는 방법과 동일. 4. 처음에는 appendFileSync를 쓰려고 했으나, 이와같은 경우 JSON.stringify과정에서 에러가 발생할 수 있다고 한다. 하여 readfile&writefile의 조합을 사용하도록 한다.

--- todo.js
+++ todo.js
@@ -1,12 +1,27 @@
 import fs from "node:fs"; // 파일 제어 도구 불러오기
 
-var count = 1;
-const task = `[ ] ${count}. ${process.argv[3]}`;
+let todolist = [];
+
+try {
+  todolist = fs.readFileSync("todo.json", "utf-8");
+  todolist = JSON.parse(todolist);
+} catch (err) {
+  fs.writeFileSync("todo.json", "[]");
+}
 
 // 1. 파일에 todo 추가하기
+let task = {
+  id: 0,
+  content: process.argv[3],
+  done: false,
+};
+
 if (process.argv[2] == "add") {
-  fs.appendFileSync("todo.txt", `${task}\n`);
-  count++;
+  let maxid = todolist[todolist.length - 1]?.id || 0;
+  task.id = maxid + 1;
+  todolist = [...todolist, task];
+  fs.writeFileSync("todo.json", JSON.stringify(todolist, null, 2));
+  console.log(`"${process.argv[3]}"가 할 일에 추가되었다.`);
 }
 
 // 2. todo 목록 조회하기
